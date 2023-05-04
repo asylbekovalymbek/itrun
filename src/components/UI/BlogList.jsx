ba22d5222ec41232699ffc22f89dc76e0f306905
Alymbek <alymbek.atay@gmail.com>
2023-05-04 17:24:22 +0600

    itrun

--- src/components/UI/BlogList.jsx
+++ src/components/UI/BlogList.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col } from "reactstrap";
 import "../../styles/blog-item.css";
 import { Link } from "react-router-dom";
-import blogData from "../../assets/data/blogData";
+import {getData} from "../../assets/data/blogData";
 
 const BlogList = () => {
+  const [blogList, setBloglist] = useState([])
+  useEffect(()=>{
+    getData().then(res => {setBloglist(res)})
+  },[])
+  
+  
   return (
     <>
-      {blogData.map((item) => (
+      {blogList.map((item) => (
         <BlogItem item={item} key={item.id} />
       ))}
     </>
@@ -31,7 +37,7 @@ const BlogItem = ({ item }) => {
               : description}
           </p>
 
-          <Link to={`/blogs/${title}`} className="read__more">
+          <Link to={`/blogs/${id}`} className="read__more">
             Узнать Больше
           </Link>
 
@@ -57,3 +63,103 @@ const BlogItem = ({ item }) => {
 };
 
 export default BlogList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Col } from "reactstrap";
+// import "../../styles/blog-item.css";
+// import { Link } from "react-router-dom";
+// import blogData from "../../assets/data/blogData";
+
+// const BlogList = () => {
+//   return (
+//     <>
+//       {blogData.map((item) => (
+//         <BlogItem item={item} key={item.id} />
+//       ))}
+//     </>
+//   );
+// };
+
+// const BlogItem = ({ item }) => {
+//   const { imgUrl, title, author, date, description, time } = item;
+
+//   return (
+//     <Col lg="4" md="6" sm="6" className="mb-5">
+//       <div className="blog__item">
+//         <img src={imgUrl} alt="" className="w-100" />
+//         <div className="blog__info p-3">
+//           <Link to={`/blogs/${title}`} className="blog__title">
+//             {title}
+//           </Link>
+//           <p className="section__description mt-3">
+//             {description.length > 100
+//               ? description.substr(0, 100)
+//               : description}
+//           </p>
+
+//           <Link to={`/blogs/${title}`} className="read__more">
+//             Узнать Больше
+//           </Link>
+
+//           <div className="blog__time pt-3 mt-3 d-flex align-items-center justify-content-between">
+//             <span className="blog__author">
+//               <i class="ri-user-line"></i> {author}
+//             </span>
+
+//             <div className=" d-flex align-items-center gap-3">
+//               <span className=" d-flex align-items-center gap-1 section__description">
+//                 <i class="ri-calendar-line"></i> {date}
+//               </span>
+
+//               <span className=" d-flex align-items-center gap-1 section__description">
+//                 <i class="ri-time-line"></i> {time}
+//               </span>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </Col>
+//   );
+// };
+
+// export default BlogList;
