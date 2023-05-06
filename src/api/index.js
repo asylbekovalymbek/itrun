a125eaa242478f958d25b49a14b694dd72463d5f
Alymbek <alymbek.atay@gmail.com>
2023-05-06 17:52:20 +0600

    itrun

---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+const rootApi = 'https://gentle-ridge-36337.herokuapp.com/api'
---
+++ src/services/auth.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { rootApi } from "../api/index"
+
+const authServices = () => {
+    const registration = (userData) => {
+        return axios.post(`${rootApi}/auth.register`, userData);   
+    };
+
+    return {
+        registration,
+    }
+}
