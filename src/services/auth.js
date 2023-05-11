f0260fa24ec0a4ef6cb9841205ef9c2e1ef34210
Bozov Myrzabek <myrzabekbozov8@gmail.com>
2023-05-11 18:07:45 +0600

    лог логаут

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,7 @@ import { rootApi } from "../api/index"
 
 const authServices = () => {
     const registration = (userData) => {
-        return axios.post(`${rootApi}/auth.register`, userData);   
+        return axios.post(`${rootApi}/auth/register`, userData);   
     };
 
     return {
